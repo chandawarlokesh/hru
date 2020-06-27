c04e5efca80b319e9f05e543b846d2d2f2ebbf16
Lokesh Chandawar <Lokesh.Chandawar@harbingergroup.com>
2020-06-27 19:45:31 +0530

    Updates

---
+++ precache-manifest.b576dd913507e563cd037c0e4a363f1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02a08909dd55129e8cfb280e22152f2",
+    "url": "/hru/index.html"
+  },
+  {
+    "revision": "e29c35be567bedd1999a",
+    "url": "/hru/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0630a431d144cfe974c2",
+    "url": "/hru/static/css/main.b80ab658.chunk.css"
+  },
+  {
+    "revision": "e29c35be567bedd1999a",
+    "url": "/hru/static/js/2.aaec9016.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/hru/static/js/2.aaec9016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0630a431d144cfe974c2",
+    "url": "/hru/static/js/main.66096eb4.chunk.js"
+  },
+  {
+    "revision": "4188200b4161673510d5",
+    "url": "/hru/static/js/runtime-main.1310ac00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91b8f5c46b46995c988f55443850a53f.js"
+  "/hru/precache-manifest.b576dd913507e563cd037c0e4a363f1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hru/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
